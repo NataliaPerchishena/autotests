23b088c0402a91bc411d081cede674ecf11b6767
Anna-Shy <55240742+Anna-Shy@users.noreply.github.com>
2020-01-29 12:14:35 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+result = docker run -it -v $PWD:/e2e -w /e2e -e CYPRESS_VIDEO cypress/included:3.2.0
+
+TOKEN=1059823581:AAFUlSWEuEubEP-2nCG6QMhR9qNopLpzQcM
+CHAT_ID=334576789
+MESSAGE="Problems in tests"
+URL="https://api.telegram.org/bot1059823581:AAFUlSWEuEubEP-2nCG6QMhR9qNopLpzQcM/sendMessage"
+
+if [$result!]
+fi curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
