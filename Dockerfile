8e7d94cc759bf90bc790c711c1e9c61641da795e
Voroshylov <voroshilov.dima@gmail.com>
2020-11-18 18:45:17 +0200

    Minor fixes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM cypress/included:5.6.0
+RUN npm install
 WORKDIR /e2e
 
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #STR_CHECK=$(ls)
-docker run -it --ipc=host -v $PWD:/e2e -w /e2e --env-file .env cypress/included:5.6.0 --spec "cypress/integration/coolers_water/automatic.spec.js"
+docker run -it --ipc=host -v $PWD:/e2e -w /e2e cypress/included:5.6.0 --spec "cypress/integration/coolers_water/automatic.spec.js"
 #TOKEN=1059823581:AAFUlSWEuEubEP-2nCG6QMhR9qNopLpzQcM
 #CHAT_ID=334576789
 #MESSAGE="Problems in tests"
