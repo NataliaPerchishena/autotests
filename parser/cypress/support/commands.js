f39228f0c8e3cdf066abc722f9d57e01431dcf34
Nataliya Perchyshena <chilly233@gmail.com>
2024-08-14 13:31:36 +0200

    commands - img log

--- parser/cypress/support/commands.js
+++ parser/cypress/support/commands.js
@@ -73,7 +73,7 @@ Cypress.Commands.add('checkPdf200', () => {
 Cypress.Commands.add('checkBasic', () => {
 
     cy.checkTitle();
-    // cy.checkDescription();
+    cy.checkDescription();
     cy.checkImage();
     cy.checkSaveAs();
     cy.checkPdf();
